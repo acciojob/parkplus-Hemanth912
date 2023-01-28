1e50d4d5b30b0a79851ddf29e4225d509a30b5a1
Hemanth912 <116891140+Hemanth912@users.noreply.github.com>
2023-01-28 17:47:06 +0530

    ms1g

--- src/main/java/com/driver/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/driver/services/impl/PaymentServiceImpl.java
@@ -1,12 +1,15 @@
 package com.driver.services.impl;
 
-import com.driver.model.Payment;
-import com.driver.model.PaymentMode;
+import com.driver.model.*;
+import com.driver.repository.ParkingLotRepository;
 import com.driver.repository.PaymentRepository;
 import com.driver.repository.ReservationRepository;
+import com.driver.repository.SpotRepository;
 import com.driver.services.PaymentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
@@ -15,8 +18,54 @@ public class PaymentServiceImpl implements PaymentService {
     @Autowired
     PaymentRepository paymentRepository2;
 
+    @Autowired
+    ParkingLotRepository parkingLotRepository;
+
+    @Autowired
+    SpotRepository spotRepository;
+
     @Override
     public Payment pay(Integer reservationId, int amountSent, String mode) throws Exception {
-        return null;
+
+        Reservation reservation = reservationRepository2.findById(reservationId).get();
+
+        Spot spot = reservation.getSpot();
+
+        int bill = reservation.getNumberOfHours()*spot.getPricePerHour();
+
+        ParkingLot parkingLot = spot.getParkingLot();
+        List<Spot> spotList = parkingLot.getSpotList();
+
+        String paymentMode = mode.toUpperCase();
+        PaymentMode[] paymentModes = PaymentMode.values();
+        Payment pay= null;
+        for(PaymentMode p : paymentModes)
+        {
+            if(paymentMode.equals(p))
+            {
+                if(bill<=amountSent)
+                {
+                    pay.setPaymentMode(p);
+                    pay.setPaymentCompleted(true);
+                    reservation.setPayment(pay);
+                    spot.setOccupied(true);
+                    for(Spot sp:spotList)
+                    {
+                        if(sp==spot)
+                            sp.setOccupied(true);
+                    }
+                    parkingLot.setSpotList(spotList);
+
+                    parkingLotRepository.save(parkingLot);
+                    paymentRepository2.save(pay);
+                    spotRepository.save(spot);
+                    reservationRepository2.save(reservation);
+                    return pay;
+                }
+                else
+                    throw new Exception("Insufficient Amount");
+            }
+        }
+        throw new Exception("Payment mode not detected");
     }
 }
